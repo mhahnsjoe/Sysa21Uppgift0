20b22e8105fd91758c4f587d956b0d9c1b926455
mhahnsjoe <56585182+mhahnsjoe@users.noreply.github.com>
2019-10-15 12:36:10 +0200

    Addition
    Added another equation

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int nummer = 2;
+		
+System.out.println("Vad är "+nummer+"+"+nummer+"?");
+System.out.println(nummer+nummer);
+
+
 	}
 
 }
